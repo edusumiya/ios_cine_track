7f2e1b1ca15824139c0467a6b9048b8287a8af7c
Eduardo Sumiya <edu.sumiya@gmail.com>
2026-03-30 11:04:51 -0300

    Revert "Feature/watch data"

---
+++ CineTrack/CineTrack/CineTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineTrackApp.swift
+//  CineTrack
+//
+//  Created by Eduardo Sumiya on 20/03/26.
+//
+
+import SwiftUI
+
+@main
+struct CineTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
